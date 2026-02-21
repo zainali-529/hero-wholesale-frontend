2dd1b62b6280acbeb043989a97aadf57040d2791
Zain Ali <zainop001@gmail.com>
2026-02-21 13:20:34 +0500

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://hero-wholesale-backend.vercel.app/api',
 });
 
 // Add a request interceptor to attach the token
